496f586891cb8677e6f0f35e22df0e39bdb2223e
nikita.klyosow <nikitka2904@gmail.com>
2018-11-28 11:03:22 +0300

    version 0.4.2 bugfix

--- app/MySql/MainCategory.php
+++ app/MySql/MainCategory.php
@@ -4,6 +4,112 @@ namespace App\MySql;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\MySql\MainCategory
+ *
+ * @property int $Subdivision_ID
+ * @property int $Catalogue_ID
+ * @property int $Parent_Sub_ID
+ * @property string $Subdivision_Name
+ * @property int|null $Template_ID
+ * @property string|null $ExternalURL
+ * @property string $EnglishName
+ * @property string $LastUpdated
+ * @property string $Created
+ * @property string|null $LastModified
+ * @property int|null $LastModifiedType
+ * @property string $Hidden_URL
+ * @property int $Read_Access_ID
+ * @property int $Write_Access_ID
+ * @property int|null $Priority
+ * @property int $Checked
+ * @property int $Edit_Access_ID
+ * @property int $Checked_Access_ID
+ * @property int $Delete_Access_ID
+ * @property int $Subscribe_Access_ID
+ * @property int $Moderation_ID
+ * @property int $Favorite
+ * @property string|null $TemplateSettings
+ * @property bool $UseMultiSubClass
+ * @property bool $UseEditDesignTemplate
+ * @property int|null $DisallowIndexing
+ * @property string|null $Description
+ * @property string|null $Keywords
+ * @property string|null $Title
+ * @property string|null $ncSMO_Title
+ * @property string|null $ncSMO_Description
+ * @property string|null $ncSMO_Image
+ * @property string|null $Language
+ * @property string $DisplayType
+ * @property string|null $LabelColor
+ * @property int $Cache_Access_ID
+ * @property int $Cache_Lifetime
+ * @property int $Comment_Rule_ID
+ * @property float|null $SitemapPriority
+ * @property string|null $SitemapChangefreq
+ * @property int|null $IncludeInSitemap
+ * @property int|null $z_id
+ * @property int|null $z_parent_id
+ * @property string|null $phone
+ * @property string|null $email
+ * @property string|null $site
+ * @property int|null $inn
+ * @property float|null $Latitude
+ * @property float|null $Longitude
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereCacheAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereCacheLifetime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereCatalogueID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereChecked($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereCheckedAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereCommentRuleID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereCreated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereDeleteAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereDisallowIndexing($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereDisplayType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereEditAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereEnglishName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereExternalURL($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereFavorite($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereHiddenURL($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereIncludeInSitemap($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereInn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereKeywords($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLabelColor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLanguage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLastModified($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLastModifiedType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLastUpdated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLatitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereLongitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereModerationID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereNcSMODescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereNcSMOImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereNcSMOTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereParentSubID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory wherePriority($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereReadAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereSite($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereSitemapChangefreq($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereSitemapPriority($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereSubdivisionID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereSubdivisionName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereSubscribeAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereTemplateID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereTemplateSettings($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereUseEditDesignTemplate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereUseMultiSubClass($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereWriteAccessID($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereZId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\MySql\MainCategory whereZParentId($value)
+ * @mixin \Eloquent
+ */
 class MainCategory extends Model
 {
     protected $connection = 'mysql';
--- app/Product.php
+++ app/Product.php
@@ -21,7 +21,7 @@ use App\Http\Resources\ProductCollection;
  * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Product query()
- * @method static \Illuminate\Database\Eloquent\Builder|Product whereFullText($search)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereFullText($search, $sorted, $page = 1, $limit = 5)
  * @mixin \Eloquent
  */
 class Product extends BaseModel
@@ -31,19 +31,49 @@ class Product extends BaseModel
     private const BASE_URL = 'http://kdvor3mkad.ru';
     private const PRODUCT_URL = 'tovar_${product}.html';
 
-    public function scopeWhereFullText($query, $search, $page = 1, $limit = 5)
+    /**
+     * @param $query
+     * @param $search
+     * @param $sorted
+     * @param int $page
+     * @param int $limit
+     * @return mixed
+     */
+    public function scopeWhereFullText($query, $search, $sorted, $page = 1, $limit = 5)
     {
         $query->getQuery()->projections = ['score' => ['$meta' => 'textScore']];
-        $query->orderBy('score', ['$meta' => 'textScore'])->orderBy('price', 'asc');
+        if ($sorted) {
+            $query->orderBy('price', 'asc');
+        } elseif ($sorted !== null) {
+            $query->orderBy('price', 'desc');
+        }
+        $query->orderBy('score', ['$meta' => 'textScore']);
         $query->skip(($page - 1) * $limit);
         $query->take($limit);
         return $query->whereRaw(['$text' => ['$search' => $search]]);
     }
 
+    /**
+     * @param $query
+     * @param $search
+     * @return mixed
+     */
+    public function scopeWhereFullTextSorted($query, $search)
+    {
+        return $query->whereRaw(['$text' => ['$search' => $search]]);
+    }
+
+    /**
+     * @param $name
+     * @param $page
+     * @param $perPage
+     * @param $sorted
+     * @return ProductResource|ProductCollection|array
+     */
     public static function findCost($name, $page, $perPage, $sorted)
     {
         $products = self::select(['name', 'desc', 'detail', 'price', 'main_category', 'ext_category', 'seller'])
-            ->whereFullText($name, $page ?? 1, $perPage ?? 5)
+            ->whereFullText($name, $sorted, $page ?? 1, $perPage ?? 5)
             ->get();
 
         $count = self::raw(function ($collection) use ($name) {
@@ -92,9 +122,13 @@ class Product extends BaseModel
         return new ProductCollection($products);
     }
 
+    /**
+     * @param $name
+     * @return ProductResource|array
+     */
     public static function findLowCost($name)
     {
-        $products = self::whereFullText($name)
+        $products = self::whereFullTextSorted($name)
             ->orderBy('price', 'asc')
             ->first();
 
@@ -105,9 +139,13 @@ class Product extends BaseModel
         return new ProductResource($products);
     }
 
+    /**
+     * @param $name
+     * @return ProductResource|array
+     */
     public static function findHighCost($name)
     {
-        $products = self::whereFullText($name)
+        $products = self::whereFullTextSorted($name)
             ->orderBy('price', 'desc')
             ->first();
 
@@ -118,7 +156,10 @@ class Product extends BaseModel
         return new ProductResource($products);
     }
 
-    public function getLink()
+    /**
+     * @return string
+     */
+    public function getLink(): string
     {
         return self::BASE_URL . $this->seller['url'] . str_replace('${product}', $this->message_id, self::PRODUCT_URL);
     }
