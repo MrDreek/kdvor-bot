cf8ac9bdb6fc9c5d948d063d80bdfb93ea76279d
Никита Клёсов <nikitka2904@gmail.com>
2019-02-09 12:23:50 +0300

    version 0.5.0 fix ext_category

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -22,7 +22,7 @@ class ImportController extends Controller
             ->where('checked', 1)
             ->get()
             ->toArray();
-        $categories = Category::get(['ext_category_name', 'ext_category_id'])->keyBy('ext_category_id')->toArray();
+        $categories = Category::get(['ext_category_name', 'ext_category_id', 'Subdivision_ID'])->keyBy('ext_category_id')->toArray();
         $main_categories = MainCategory::get(['Subdivision_ID', 'Subdivision_Name', 'Parent_Sub_ID'])->keyBy('Subdivision_ID')->toArray();
         $sellers = Seller::get(['Subdivision_Name', 'Hidden_URL', 'phone', 'email', 'site', 'Subdivision_ID', 'Parent_Sub_ID'])->where('Parent_Sub_ID', Seller::PARENT_SUB_ID)->keyBy('Subdivision_ID')->toArray();
 
@@ -35,7 +35,12 @@ class ImportController extends Controller
             $item->price = $product['Price'];
             $item->keyword = $product['Keyword'];
             $item->ext_offer_url = $product['ext_offer_url'];
-            $item->ext_category = $categories[$product['ext_category_id']]['ext_category_name'] ?? null;
+
+            $ext_category = array_filter($categories, function ($item, $key) use ($product){
+                return $item['ext_category_id'] === $product['ext_category_id'] && $item['Subdivision_ID'] === $product['Subdivision_ID'];
+            }, ARRAY_FILTER_USE_BOTH);
+
+            $item->ext_category = array_values($ext_category)[0]['ext_category_name'] ?? null;
             $item->main_category = $main_categories[$product['int_category_id']]['Subdivision_Name'] ?? null;
             $item->seller = [
                 'seller_name' => $sellers[$product['Subdivision_ID']]['Subdivision_Name'],
